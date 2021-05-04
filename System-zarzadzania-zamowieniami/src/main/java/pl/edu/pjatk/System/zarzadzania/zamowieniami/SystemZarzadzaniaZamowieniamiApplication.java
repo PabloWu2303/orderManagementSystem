28d83f5721185ebe3afb08bf3fbccb419a223583
PabloWu2303 <pawelwawrzenczyk@gmail.com>
2021-05-04 12:00:09 +0200

    Inicjalizacja projektu z wykorzystaniem SpringBoot

---
+++ System-zarzadzania-zamowieniami/src/main/java/pl/edu/pjatk/System/zarzadzania/zamowieniami/SystemZarzadzaniaZamowieniamiApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pjatk.System.zarzadzania.zamowieniami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemZarzadzaniaZamowieniamiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemZarzadzaniaZamowieniamiApplication.class, args);
+	}
+
+}
